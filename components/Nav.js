4c7b6b392e0f88b44f77d1cbb793f36dd8676e92
ronan <ronan@thenextweb.com>
2022-08-23 13:55:47 +0200

    update Nav with hover effects

--- components/Nav.js
+++ components/Nav.js
@@ -3,12 +3,12 @@ import Link from 'next/link';
 const Nav = () => {
     return(
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link href={'/'}><a className="mr-5 hover:text-gray-900">Home</a></Link>
-            <Link href={'/about'}><a className="mr-5 hover:text-gray-900">About</a></Link>
-            <Link href={'/artists'}><a className="mr-5 hover:text-gray-900">Artists</a></ Link>
-            <Link href={'/schedule'}><a className="mr-5 hover:text-gray-900">Schedule</a></ Link>
-            <Link href={'/tickets'}><a className="mr-5 hover:text-gray-900">Tickets</a></ Link>
-            <Link href={'/contact'}><a className="mr-5 hover:text-gray-900">Contact</a></Link>
+            <Link href={'/'}><a className="mr-5 hover:text-gray-100">Home</a></Link>
+            <Link href={'/about'}><a className="mr-5 hover:text-gray-100">About</a></Link>
+            <Link href={'/artists'}><a className="mr-5 hover:text-gray-100">Artists</a></ Link>
+            <Link href={'/schedule'}><a className="mr-5 hover:text-gray-100">Schedule</a></ Link>
+            <Link href={'/tickets'}><a className="mr-5 hover:text-gray-100">Tickets</a></ Link>
+            <Link href={'/contact'}><a className="mr-5 hover:text-gray-100">Contact</a></Link>
         </nav>
     )    
 }
