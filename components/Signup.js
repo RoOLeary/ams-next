e833f4eb38456ef6a69a391705c903ca3c419c56
ronan <ronan@thenextweb.com>
2022-08-24 16:37:05 +0200

    tsx on index

--- components/Signup.js
+++ components/Signup.js
@@ -9,11 +9,11 @@ const Signup = () => {
                 </div>
                 <div className="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
                 <div className="relative flex-grow w-full">
-                    <label htlfFor="full-name" className="leading-7 text-sm text-gray-100">Full Name</label>
+                    <label htmlfFor="full-name" className="leading-7 text-sm text-gray-100">Full Name</label>
                     <input type="text" id="full-name" name="full-name" className="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
                 <div className="relative flex-grow w-full">
-                    <label htlfFor="email" className="leading-7 text-sm text-gray-100">Email</label>
+                    <label htmlfFor="email" className="leading-7 text-sm text-gray-100">Email</label>
                     <input type="email" id="email" name="email" className="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
                 <button className="text-white bg-goldenrod border-0 py-2 px-8 focus:outline-none hover:white rounded text-lg">Submit</button>
