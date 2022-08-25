6f3cd64327c0d7f43c2cdb127c187f8dcc4411f1
ronan <ronan@thenextweb.com>
2022-08-25 10:34:47 +0200

    tweak animation of FAQ component

--- components/Faqs/FaqLayout.js
+++ components/Faqs/FaqLayout.js
@@ -35,7 +35,7 @@ const FaqLayout = ({ title, children, index, activeIndex, setActiveIndex }) => {
             {(activeIndex === index) && (
                 <motion.div
                     animate={{ opacity: [0, 1] }}
-                    transition={{ times: [0, 1], duration: 0.25 }}
+                    transition={{ times: [0, 1], duration: 0.5 }}
                     className="flex w-full bg-lightgray-200 p-4 mb-6"
                 >
                     {children}
