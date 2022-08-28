12ed0d48347ee86e66a2ee1cd5484ec5012338d5
Ronan <roleary81@gmail.com>
2022-08-28 09:48:09 +0200

    remove nav class on route change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,30 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { AnimatePresence } from 'framer-motion'
 
+
+
 function AlgarveMusicSeries({ Component, pageProps }: AppProps): JSX.Element {
+
+  const router = useRouter()
+
+  useEffect(() => {
+
+    const handleStart = () => {
+      console.log(`Loading...`)
+      document.body.className = '';
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+    }
+  }, [router])
+
   return (
     <AnimatePresence
         mode='wait'
