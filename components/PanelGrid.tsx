5c07039303cdc9dae7666e700d61b308bee3da84
Ronan <roleary81@gmail.com>
2022-08-28 14:26:21 +0200

    fixes and such

--- components/PanelGrid.tsx
+++ components/PanelGrid.tsx
@@ -17,11 +17,11 @@ const PanelGrid = ({ panelGridTitle }:Panel) => {
                 <div className="flex flex-wrap -m-4">
                     <div className="lg:w-1/3 sm:w-1/2 p-2">
                         <div className="flex relative">
-                            <Image loader={imageLoader} alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://source.unsplash.com/600x450/?cello" layout="fill"  />
+                            <Image loader={imageLoader} alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://source.unsplash.com/600x450/?faro" layout="fill"  />
                             <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100 transition-opacity hover:cursor-pointer">
-                                <h2 className="tracking-widest text-sm title-font font-medium gold mb-1">THE SUBTITLE</h2>
-                                <h1 className="title-font text-lg font-medium text-black mb-3">Shooting Stars</h1>
-                                <p className="leading-relaxed text-gray-400">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                                <h2 className="tracking-widest text-sm title-font font-medium gold mb-1">REGION</h2>
+                                <h1 className="title-font text-lg font-medium text-black mb-3">Faro</h1>
+                                <p className="leading-relaxed text-gray-400">Faro is a municipality, the southernmost city and capital of the district of the same name in Algarve, Portugal.</p>
                             </div>
                         </div>
                     </div>
@@ -57,7 +57,7 @@ const PanelGrid = ({ panelGridTitle }:Panel) => {
                     </div>
                     <div className="lg:w-1/3 sm:w-1/2 p-2">
                         <div className="flex relative">
-                            <Image loader={imageLoader} alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://source.unsplash.com/600x450/?piano" layout="fill"  />
+                            <Image loader={imageLoader} alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://source.unsplash.com/600x450/?orchestra" layout="fill"  />
                             <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100 transition-opacity hover:cursor-pointer">
                                 <h2 className="tracking-widest text-sm title-font font-medium gold mb-1">THE SUBTITLE</h2>
                                 <h1 className="title-font text-lg font-medium text-black mb-3">Holden Caulfield</h1>
@@ -67,7 +67,7 @@ const PanelGrid = ({ panelGridTitle }:Panel) => {
                     </div>
                     <div className="lg:w-1/3 sm:w-1/2 p-2">
                         <div className="flex relative">
-                            <Image loader={imageLoader} alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://source.unsplash.com/600x450/?classical?music" layout="fill"  />
+                            <Image loader={imageLoader} alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://source.unsplash.com/600x450/?clarinet" layout="fill"  />
                             <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100 transition-opacity hover:cursor-pointer">
                                 <h2 className="tracking-widest text-sm title-font font-medium gold mb-1">THE SUBTITLE</h2>
                                 <h1 className="title-font text-lg font-medium text-black mb-3">Alper Kamu</h1>
@@ -87,7 +87,7 @@ const PanelGrid = ({ panelGridTitle }:Panel) => {
                     </div>
                     <div className="lg:w-1/3 sm:w-1/2 p-2">
                         <div className="flex relative">
-                            <Image loader={imageLoader} alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://source.unsplash.com/600x450/?piano" layout="fill"  />
+                            <Image loader={imageLoader} alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://source.unsplash.com/600x450/?harp" layout="fill"  />
                             <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100 transition-opacity hover:cursor-pointer">
                                 <h2 className="tracking-widest text-sm title-font font-medium gold mb-1">THE SUBTITLE</h2>
                                 <h1 className="title-font text-lg font-medium text-black mb-3">Holden Caulfield</h1>
@@ -97,7 +97,7 @@ const PanelGrid = ({ panelGridTitle }:Panel) => {
                     </div>
                     <div className="lg:w-1/3 sm:w-1/2 p-2">
                         <div className="flex relative">
-                            <Image loader={imageLoader} alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://source.unsplash.com/600x450/?classical?music" layout="fill"  />
+                            <Image loader={imageLoader} alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://source.unsplash.com/600x450/?viola" layout="fill"  />
                             <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100 transition-opacity hover:cursor-pointer">
                                 <h2 className="tracking-widest text-sm title-font font-medium gold mb-1">THE SUBTITLE</h2>
                                 <h1 className="title-font text-lg font-medium text-black mb-3">Alper Kamu</h1>
