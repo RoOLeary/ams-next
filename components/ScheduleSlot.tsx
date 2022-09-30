79e9e0ae1254e2c864ff656b0bae5c1d6495082a
Ronan <roleary81@gmail.com>
2022-09-30 10:53:23 +0200

    update hover colour on scheduleslots

--- components/ScheduleSlot.tsx
+++ components/ScheduleSlot.tsx
@@ -1,4 +1,10 @@
 import Link from 'next/link';
+import styled from 'styled-components';
+
+
+const LinkText = styled.span`
+    color: goldenrod;
+`;
 
 
 interface ISched {
@@ -33,9 +39,9 @@ const ScheduleSlot = ({ id, title, date, time, price, ticketLink, excerpt, detai
             <p className="leading-relaxed">{excerpt}</p>
             {ticketLink ? 
                 ticketLink.includes("@") ?
-                <div>Click <a href={`mailto:${ticketLink}`} target="_blank" rel="noreferrer">here</a> to get seats.</div>  
+                <div>Click <a href={`mailto:${ticketLink}`} target="_blank" rel="noreferrer"><span className="gold">here</span></a> to get seats.</div>  
                 :      
-                <div>Click <a href={`${ticketLink}`} target="_blank" rel="noreferrer">here</a> to get seats.</div>        
+                <div>Click <a href={`${ticketLink}`} target="_blank" rel="noreferrer"><span className="gold">here</span></a> to get seats.</div>        
             : ""}
             {/* <Link href={`schedule/event/${slug}`}>
                 <a className="gold inline-flex items-center mt-4">More Info
